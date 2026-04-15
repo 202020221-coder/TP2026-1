b458cf2fa4fe68d27be2bcefc7419eef80086421
Edgar Franco <just.me.edgar.2004@gmail.com>
2026-04-14 22:01:41 -0500

    feat: add loading state during mock submit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Toaster } from "sonner";
+import {
+  CircleCheckBig,
+  CircleX,
+  Info,
+  Loader,
+  TriangleAlert,
+} from "lucide-react";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <Toaster
+      icons={{
+        success: <CircleCheckBig />,
+        info: <Info />,
+        warning: <TriangleAlert />,
+        error: <CircleX />,
+        loading: <Loader className="animate-spin" />,
+      }}
+      position="top-center"
+      richColors
+      
+    />
   </StrictMode>,
-)
+);
