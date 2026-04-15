df1885b44a78df93d3394e85111942d53e7d7742
Edgar Franco <just.me.edgar.2004@gmail.com>
2026-04-14 22:20:29 -0500

    fix: correct Orders page title

--- src/solicitudes/pages/ListOrdersPage.tsx
+++ src/solicitudes/pages/ListOrdersPage.tsx
@@ -5,7 +5,7 @@ export function ListOrdersPage () {
   return (
     <div className="p-8 space-y-6">
       <h1 className="text-2xl font-semibold text-gray-800">
-        Listado de Cotizaciones
+        Listado de Solicitudes
       </h1>
       <ListOrdersProvider>
         <div className="bg-white p-6 rounded-xl shadow-none border">
