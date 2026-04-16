268130585ec55190c4ef0d48f5d48b4df58250d2
Edgar Franco <just.me.edgar.2004@gmail.com>
2026-04-16 18:39:04 -0500

    hotfix: fix redirection when creating order

--- src/solicitudes/pages/create-request/CreateRequestPage.tsx
+++ src/solicitudes/pages/create-request/CreateRequestPage.tsx
@@ -516,7 +516,7 @@ export function CreateRequestPage() {
                                     selectedCatalogItems,
                                     preferencesData,
                                 });
-                                navigate('/intranet/solicitudes/', { replace: true });
+                                navigate('/intranet/solicitudes/mis-solicitudes', { replace: true });
                             }
                         }}
                         className={`px-8 py-2 rounded-lg font-semibold transition-all ${(currentStep === 1 && !clientType)
