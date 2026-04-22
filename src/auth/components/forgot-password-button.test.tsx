7c143bacae382723a88bea2be4cd2e3960c09c40
Edgar Franco <just.me.edgar.2004@gmail.com>
2026-04-22 00:34:36 -0500

    chore: improve code legibility

--- src/auth/components/forgot-password-button.test.tsx
+++ src/auth/components/forgot-password-button.test.tsx
@@ -1,34 +1,32 @@
-import { describe, it, expect, beforeEach } from "vitest";
-import { cleanup, render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
 import ForgotPasswordButton from "./forgot-password-button";
 import { RecoverPasswordProvider } from "../context/recover-password-context";
 import userEvent from "@testing-library/user-event";
 
-beforeEach(() => {
-  cleanup();
-});
-
 describe("Forgor Password Button", () => {
   it("Renders a button to recover password", async () => {
-    render(
-      <RecoverPasswordProvider>
-        <ForgotPasswordButton />
-      </RecoverPasswordProvider>,
-    );
+    renderButton();
     const button = screen.getByText("¿Has olvidado tu contraseña?");
     expect(button).toBeInTheDocument();
   });
 
   it("Opens dialog on click", async () => {
-    render(
-      <RecoverPasswordProvider>
-        <ForgotPasswordButton />
-      </RecoverPasswordProvider>,
-    );
-
+    renderButton();
     const button = screen.getByText("¿Has olvidado tu contraseña?");
     await userEvent.click(button);
-    const dialog = screen.getByTestId("recover-password-dialog")
+    const dialog = screen.getByTestId("recover-password-dialog");
     expect(dialog).toBeVisible();
   });
 });
+
+/**
+ * Renders ``ForgotPasswordButton.tsx``.
+ */
+function renderButton() {
+  render(
+    <RecoverPasswordProvider>
+      <ForgotPasswordButton />
+    </RecoverPasswordProvider>,
+  );
+}
