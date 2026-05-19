d17b793fa3547984d14a3bc13b0cce8f004d5332
Edgar Franco <leivachavezedgarfranco@gmail.com>
2026-05-19 18:46:50 -0500

    fix: fix build

--- src/intranet/request/context/ListRequestsProvider.tsx
+++ src/intranet/request/context/ListRequestsProvider.tsx
@@ -1,32 +1,32 @@
-import { useState, type FC, type ReactNode } from "react";
-import type { GetRequestsQP } from "../interfaces";
-import { useQuery } from "@tanstack/react-query";
-import { GetAllRequest } from "../api/request.api";
-import { ListRequestContext } from "./ListRequestContext";
+// import { useState, type FC, type ReactNode } from "react";
+// import type { GetRequestsQP } from "../interfaces";
+// import { useQuery } from "@tanstack/react-query";
+// import { GetAllRequest } from "../api/request.api";
+// import { ListRequestContext } from "./ListRequestContext";
 
-export const ListRequestsProvider: FC<{ children: ReactNode }> = ({
-    children,
-}) => {
-    const [queryParams, setQueryParams] = useState<GetRequestsQP>({
-        page: 1,
-        per_page: 10,
-    });
-    const result = useQuery({
-        queryKey: ["requests", queryParams],
-        queryFn: () => GetAllRequest(queryParams),
-    });
+// export const ListRequestsProvider: FC<{ children: ReactNode }> = ({
+//     children,
+// }) => {
+//     const [queryParams, setQueryParams] = useState<GetRequestsQP>({
+//         page: 1,
+//         per_page: 10,
+//     });
+//     const result = useQuery({
+//         queryKey: ["requests", queryParams],
+//         queryFn: () => GetAllRequest(queryParams),
+//     });
 
-    const query = (queryParams: GetRequestsQP) => {
-        setQueryParams(queryParams);
-      };
+//     const query = (queryParams: GetRequestsQP) => {
+//         setQueryParams(queryParams);
+//       };
     
-    console.log(result.data);
+//     console.log(result.data);
 
-    return (
-        <ListRequestContext.Provider value={{ result, query, queryParams }}>
-            {children}
-        </ListRequestContext.Provider>
-    );
-};
+//     return (
+//         <ListRequestContext.Provider value={{ result, query, queryParams }}>
+//             {children}
+//         </ListRequestContext.Provider>
+//     );
+// };
 
-export default ListRequestsProvider;
+// export default ListRequestsProvider;
