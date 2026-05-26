4682d4be5498fbd7b7437022be595e3fc784b731
Edgar Franco <leivachavezedgarfranco@gmail.com>
2026-05-26 17:56:09 -0500

    refactor: remove unused NegotiationMessage interface and related driver query parameters

--- src/intranet/quotation/interfaces/query-params.dto.ts
+++ src/intranet/quotation/interfaces/query-params.dto.ts
@@ -5,15 +5,10 @@ export interface GetInventoryItemsQP {
   limit?: number;
 }
 
-export interface GetAvailableDriversQP {
-  fecha?: string;
-}
-
 export interface GetAvailableTrucksQP {
   page?: number;
   limit?: number;
 }
-
 
 export interface GetQuotationQP {
   page?: number;
